e85a9a3f89243e8f99a24f0ec2bd658199779f71
DaniduWijendra <daniduwijendra@gmail.com>
2020-03-26 19:06:04 +0530

    My new

--- product.php
+++ product.php
@@ -13,21 +13,21 @@
     <div class="row" align="center">
       <?php  for($x=0 ; $x<9 ; $x++){  ?>
     <div class="col-md-4 sm-12">
-      <div class="card btn " id="product" style="margin:20px; padding:5px;">
+      <div class="card btn" id="product" style="margin:20px; padding:5px;">
       <a href="product_details.php">  <img src="images\unnamed.jpg" class="card-img-top" alt="..."></a>
           <div class="card-body">
-            <h4><a href="product_details.php">Muffins Cream Sweets Dessert Rose Flower</a></h4>
-          <p class="card-text"><a href="product_details.php">You know what I love getting even more than flowers? Pretty cupcakes. If you want to give your sweetie roses for Valentine’s Day, but don’t want to pay ar
-            tificially high prices, consi .</a></p>
+            <h4><a href="product_details.php" style="text-decoration:none">Muffins Cream Sweets Dessert Rose Flower</a></h4>
+          <p class="card-text">You know what I love getting even more than flowers? Pretty cupcakes. If you want to give your sweetie roses for Valentine’s Day, but don’t want to pay ar
+            tificially high prices, consi .</p>
             <div class="row">
-                <label class="col-6" ><a href="product_details.php?pid=1" ><span class="
+                <label class="col-6" ><span class="
                   color: #fff;
-                  text-decoration: none;">price</span></a></label>
-                <label class="col-6"><a href="product_details.php">Rs 500.00/= </a></label>
+                  text-decoration: none;">price</span></label>
+                <label class="col-6">Rs 500.00/=</label>
             </div>
             <div class="row">
-              <button class="col-6 btn-primary" type="submit" name="button">Buy</button>
-              <button class="col-6 btn-primary" type="button" name="button">Favorirs</button>
+                <button type="button" style="width:100%" class="btn btn-outline-primary">Add to Cart</button>
+              <button type="button" style="width:100%" class="btn btn-outline-primary">Buy</button>
             </div>
         </div>
     </div>
